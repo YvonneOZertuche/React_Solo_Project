022ed5b72e6c69cfa3e96e8df86c21d2fa151228
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-10-04 15:15:44 -0500

    weather.json

---
+++ solo_project/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react"
+import axios from "axios"
+
+const App = () => {
+  // const url = "https://api.openweathermap.org/data/2.5/weather?q=austin&appid=dca2046d1080c39c6cc5f47a523a8fbc"
+
+  return <></>
+}
+
+export default App
